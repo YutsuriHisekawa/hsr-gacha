a9d8b6a12a5082e9f2a07bec131c4bd30381fc6b
Hisekawa <tetsuyakumiko01@gmail.com>
2023-08-31 20:51:55 +0700

    Gacha

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
+<script setup>
+import gacha from './components/Gacha.vue';
+</script>
+
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="p-4 bg-white rounded shadow-lg max-w-lg w-full">
-      <Gacha />
+      <gacha />
     </div>
   </div>
 </template>
-
-<script>
-import Gacha from './components/Gacha.vue';
-
-export default {
-  components: {
-    Gacha,
-  },
-};
-</script>
-
-<style>
-/* Anda bisa menambahkan styling global disini jika perlu */
-</style>
