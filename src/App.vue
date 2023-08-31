94293a273464a4a58268fedbd47d292a47accd42
Hisekawa <tetsuyakumiko01@gmail.com>
2023-08-31 21:01:58 +0700

    Judi

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import gacha from './components/judi.vue';
+import judi from './components/judi.vue';
 </script>
 
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="p-4 bg-white rounded shadow-lg max-w-lg w-full">
-      <gacha />
+      <judi></judi>
     </div>
   </div>
 </template>
