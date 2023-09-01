57094d17c43c0cbfaff5727833363706b3df469f
Hisekawa <tetsuyakumiko01@gmail.com>
2023-09-01 08:01:59 +0700

    Judi

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ import bar from './components/bar.vue';
 
 <template>
   <bar />
-  <div class="min-h-screen w-full flex items-center justify-center bg-gray-100 dark:bg-gray-950">
+  <div
+    class="min-h-screen w-full flex items-center justify-center bg-gray-100 dark:bg-gray-950 pt-36 pb-36">
     <div
       class="p-4 ml-5 mr-5 bg-white dark:bg-gray-600 rounded shadow-lg max-w-3xl w-full mt-5 mb-5">
       <judi></judi>
